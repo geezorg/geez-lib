8ce3be822b5c4ba0bca52b9b1396fa0d5c0e3489
dyacob <yacob@geez.org>
2019-10-11 15:12:56 -0400

    starting GeezNet, updating GeezTypeNet

--- src/main/java/org/geez/convert/fontsystem/ConvertFontSystemGeezType.java
+++ src/main/java/org/geez/convert/fontsystem/ConvertFontSystemGeezType.java
@@ -20,13 +20,16 @@ public class ConvertFontSystemGeezType extends ConvertFontSystemDiacriticalSyste
 	}
 	
 	private void init() {
-		this.initialize( "monodirectional/GeezTypeNet.txt", "GeezTypeNet" );
+		this.initialize( "biirectional/GeezType.txt", "GeezType" );
 		
 		huletNeteb = ':';
 		
 		diacritics.addAll (
-			Arrays.asList( "\u00e8", "\u00e9", "\u00ea", "\u00eb", "\u00ec", "\u00ed", "\u00ee", "\u00ef", "\u00f0", "\u00f1", 
-					"\u00f2", "\u00f3", "\u00f4", "\u00f5", "\u00f6", "\u00f7", "\u00f8", "\u00f9", "\u00fa", "\u00fb", "\u00fc"  )
+			Arrays.asList( 
+					"\u00e8", "\u00e9", "\u00ea", "\u00eb", "\u00ec", "\u00ed", "\u00ee", "\u00ef",
+					"\u00f0", "\u00f1", "\u00f2", "\u00f3", "\u00f4", "\u00f5", "\u00f6", "\u00f7", 
+					"\u00f8", "\u00f9", "\u00fa", "\u00fb", "\u00fc", "\u00fd", "\u00fe", "\u00ff"
+			)
 		);
 		
 		translit2 = null;
@@ -37,7 +40,7 @@ public class ConvertFontSystemGeezType extends ConvertFontSystemDiacriticalSyste
 	
 	/*
 	public boolean isMacron(String text) {
-		if ( text.equals( "\u0068" ) ) {
+		if ( text.equals( "\u007a" ) ) {
 			return true;
 		}
 		return false;
--- src/main/java/org/geez/convert/fontsystem/ConvertFontSystemGeezTypeNet.java
+++ src/main/java/org/geez/convert/fontsystem/ConvertFontSystemGeezTypeNet.java
@@ -25,8 +25,11 @@ public class ConvertFontSystemGeezTypeNet extends ConvertFontSystemDiacriticalSy
 		huletNeteb = ':';
 		
 		diacritics.addAll (
-			Arrays.asList( "\u00e8", "\u00e9", "\u00ea", "\u00eb", "\u00ec", "\u00ed", "\u00ee", "\u00ef", "\u00f0", "\u00f1", 
-					"\u00f2", "\u00f3", "\u00f4", "\u00f5", "\u00f6", "\u00f7", "\u00f8", "\u00f9", "\u00fa", "\u00fb", "\u00fc"  )
+			Arrays.asList( 
+					"\u00e8", "\u00e9", "\u00ea", "\u00eb", "\u00ec", "\u00ed", "\u00ee", "\u00ef",
+					"\u00f0", "\u00f1", "\u00f2", "\u00f3", "\u00f4", "\u00f5", "\u00f6", "\u00f7", 
+					"\u00f8", "\u00f9", "\u00fa", "\u00fb", "\u00fc", "\u00fd", "\u00fe", "\u00ff"
+			)
 		);
 		
 		translit2 = null;
@@ -37,7 +40,7 @@ public class ConvertFontSystemGeezTypeNet extends ConvertFontSystemDiacriticalSy
 	
 	/*
 	public boolean isMacron(String text) {
-		if ( text.equals( "\u0068" ) ) {
+		if ( text.equals( "\u0068" ) || text.equals( "\u007a" ) || text.equals( "\u008d" ) ) {
 			return true;
 		}
 		return false;
