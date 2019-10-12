43fd9058a1173e7872d94e2cc6c604061bc3fa92
dyacob <yacob@geez.org>
2019-10-11 22:37:30 -0400

    fix to diacritical addresses

--- src/main/java/org/geez/convert/fontsystem/ConvertFontSystemGeezType.java
+++ src/main/java/org/geez/convert/fontsystem/ConvertFontSystemGeezType.java
@@ -26,9 +26,9 @@ public class ConvertFontSystemGeezType extends ConvertFontSystemDiacriticalSyste
 		
 		diacritics.addAll (
 			Arrays.asList( 
-					"\u00e8", "\u00e9", "\u00ea", "\u00eb", "\u00ec", "\u00ed", "\u00ee", "\u00ef",
-					"\u00f0", "\u00f1", "\u00f2", "\u00f3", "\u00f4", "\u00f5", "\u00f6", "\u00f7", 
-					"\u00f8", "\u00f9", "\u00fa", "\u00fb", "\u00fc", "\u00fd", "\u00fe", "\u00ff"
+					"\uf0e8", "\uf0e9", "\uf0ea", "\uf0eb", "\uf0ec", "\uf0ed", "\uf0ee", "\uf0ef",
+					"\uf0f0", "\uf0f1", "\uf0f2", "\uf0f3", "\uf0f4", "\uf0f5", "\uf0f6", "\uf0f7", 
+					"\uf0f8", "\uf0f9", "\uf0fa", "\uf0fb", "\uf0fc", "\uf0fd", "\uf0fe", "\uf0ff"
 			)
 		);
 		
