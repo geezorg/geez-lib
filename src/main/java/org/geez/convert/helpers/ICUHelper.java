0f77ed54261145b80f552b57fcdf3366a9e5b794
dyacob <yacob@geez.org>
2019-10-15 08:15:39 -0400

    trying to set XML encoding in an InputSource

--- src/main/java/org/geez/convert/helpers/ICUHelper.java
+++ src/main/java/org/geez/convert/helpers/ICUHelper.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -123,9 +124,13 @@ public class ICUHelper {
 			// this is a week test for a path hierarchy, we assume some path for a user defined file
 			rulesFileXML = resourcePath + rulesFileXML ; 
 		}
-		InputStream xmlStream = classLoader.getResourceAsStream( rulesFileXML );    
+		InputStream xmlStream = classLoader.getResourceAsStream( rulesFileXML );
+        Reader reader = new InputStreamReader(xmlStream,"UTF-8");
+        InputSource xmlSource = new InputSource(reader);
+        xmlSource.setEncoding("UTF-8");
+		
 	    
-	    Document doc = builder.parse( xmlStream );
+	    Document doc = builder.parse( xmlSource );
 	    NodeList nodes = doc.getElementsByTagName( "tRule" );
 	    Element  element = (Element) nodes.item(0); // assume only one
 	    
